6b60ce4124c3352850e7d6f1da6fc3445b404f36
LUKE PLOURDE <lukeplourde@hotmail.com>
2016-10-18 13:49:54 -0400

    Exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,11 @@
+# puts 55 * 0.20
+
+
+# puts "Hello World! " + 2.to_s
+
+
+ puts "The answer to 45628 * 7839 is :   #{45628 * 7839}"
+
+
+# puts (10 < 20 && 30 < 20) || !(10 == 11)
+# Will evaluate "true".
